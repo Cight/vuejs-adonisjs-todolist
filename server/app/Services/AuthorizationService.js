b7e998c05ed82a614c52f6e751d256af18ef90be
cight <necronezum@gmail.com>
2022-12-20 05:53:39 -0500

    invalid access auth exception

--- server/app/Services/AuthorizationService.js
+++ server/app/Services/AuthorizationService.js
@@ -1,8 +1,12 @@
+const InvalidAccessException = use('App/Exceptions/InvalidAccessException')
+
 // invalid access exception
 class AuthorizationService {
     verifyPermission(resource, user) {
         if (resource.user_id !== user.id) {
-            throw new Error()
+            throw new InvalidAccessException()
         }
     }
 }
+
+module.exports = new AuthorizationService()
