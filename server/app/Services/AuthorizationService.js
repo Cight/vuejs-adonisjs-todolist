4e6fdca8725c08997a154094263d0cac900f80e6
cight <necronezum@gmail.com>
2022-12-22 05:15:26 -0500

    new exception

--- server/app/Services/AuthorizationService.js
+++ server/app/Services/AuthorizationService.js
@@ -1,8 +1,13 @@
 const InvalidAccessException = use('App/Exceptions/InvalidAccessException')
+const ResourceNotExistException = use('App/Exceptions/ResourceNotExistException')
 
 // invalid access exception
 class AuthorizationService {
     verifyPermission(resource, user) {
+        if (!resource) {
+            throw new ResourceNotExistException()
+        }
+
         if (resource.user_id !== user.id) {
             throw new InvalidAccessException()
         }
